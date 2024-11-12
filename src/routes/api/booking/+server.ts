ddc93f30b1a297fb8b2964f89ab46e15844cabe1
nils <penzel.nils@web.de>
2024-11-12 23:37:30 +0100

    wip

--- src/routes/api/booking/+server.ts
+++ src/routes/api/booking/+server.ts
@@ -54,26 +54,28 @@ export async function POST(event: RequestEvent) {
 		if (firstConnection == undefined) {
 			return json({ message: 'Die erste Anfrage kann nicht erfüllt werden.' }, { status: 400 });
 		}
-		if (parameters.connection2 != null) {
-			const secondConnection = (
-				await white(
-					parameters.connection2.start.coordinates,
-					[
-						{
-							coordinates: parameters.connection2.target.coordinates,
-							times: [new Date(parameters.connection2.targetTime)]
-						}
-					],
-					parameters.capacities,
-					false,
-					trx
-				)
-			)[0][0];
-			if (secondConnection == undefined) {
-				return json({ message: 'Die zweite Anfrage kann nicht erfüllt werden.' }, { status: 400 });
-			}
-			insertRequest(secondConnection, parameters.capacities, parameters.connection2, customer.id);
+		if (parameters.connection2 == null) {
+			insertRequest(firstConnection, parameters.capacities, parameters.connection1, customer.id);
+			return json([]);
 		}
+		const secondConnection = (
+			await white(
+				parameters.connection2.start.coordinates,
+				[
+					{
+						coordinates: parameters.connection2.target.coordinates,
+						times: [new Date(parameters.connection2.targetTime)]
+					}
+				],
+				parameters.capacities,
+				false,
+				trx
+			)
+		)[0][0];
+		if (secondConnection == undefined) {
+			return json({ message: 'Die zweite Anfrage kann nicht erfüllt werden.' }, { status: 400 });
+		}
+		insertRequest(secondConnection, parameters.capacities, parameters.connection2, customer.id);
 		insertRequest(firstConnection, parameters.capacities, parameters.connection1, customer.id);
 	});
 	return json([]);
