e49842a4fc7826f95ac6016e98da23faa7a671e7
nils <penzel.nils@web.de>
2025-07-05 02:23:59 +0200

    wip

--- scripts/reproduce/callBookingApi.ts
+++ scripts/reproduce/callBookingApi.ts
@@ -4,29 +4,29 @@ import 'dotenv/config';
 import { bookingApi } from '../../src/lib/server/booking/bookingApi';
 
 const parameters = {
-	connection1: {
-		start: {
-			lng: 14.7548758,
-			lat: 51.5010823,
-			address: 'Brandstraße 9'
-		},
-		target: {
-			lng: 14.5289849,
-			lat: 51.5353088,
-			address: 'Siedlung - Sydlišćo'
-		},
-		startTime: 1751216212673,
-		targetTime: 1751219410160,
-		signature: '',
-		startFixed: true
-	},
-	connection2: null,
-	capacities: {
-		passengers: 1,
-		bikes: 0,
-		luggage: 0,
-		wheelchairs: 0
-	}
+  "connection1": {
+    "start": {
+      "lng": 14.4915569,
+      "lat": 51.4575301,
+      "address": "Körnerplatz"
+    },
+    "target": {
+      "lng": 14.5295784,
+      "lat": 51.5344271,
+      "address": "Mühlroser Straße 3"
+    },
+    "startTime": 1752836548153,
+    "targetTime": 1752838406153,
+    "signature": "",
+    "startFixed": true
+  },
+  "connection2": null,
+  "capacities": {
+    "passengers": 2,
+    "bikes": 0,
+    "luggage": 0,
+    "wheelchairs": 0
+  }
 };
 
 const kidsThreeToFour = 0;
