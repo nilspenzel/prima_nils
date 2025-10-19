42b33b587f3689381935098ed9d33f9ccb624d63
nils <penzel.nils@web.de>
2025-10-19 23:17:43 +0200

    wip

--- scripts/createPairs.ts
+++ scripts/createPairs.ts
@@ -2,11 +2,11 @@ import type { Coordinates } from '../src/lib/util/Coordinates.js';
 import * as fs from 'fs';
 import * as readline from 'readline';
 import { batchOneToManyCarRouting } from '../src/lib/server/util/batchOneToManyCarRouting.js';
-import { MINUTE } from '../src/lib/util/time.js';
+import { HOUR, MINUTE } from '../src/lib/util/time.js';
 
 async function main() {
     const prevEvent = {lat: 51.5446031, lng: 14.5355952, leeway: 60};
-    const nextEvent = {lat: 51.454776, lng: 14.95765, leeway: 3000};
+    const nextEvent = {lat: 51.454776, lng: 14.95765, leeway: 4000};
 
     const coordinatesPromise = processCoordinates([prevEvent, nextEvent]);
 
@@ -51,16 +51,24 @@ async function processCoordinates(t: (Coordinates&{leeway:number})[]): Promise<{
     const pCoordinates = coordinates.filter((_,idx) => possible[idx]);
    const pForward = forward.filter((_,idx) => possible[idx]);
    const pBackward = backward.filter((_,idx) => possible[idx]);
-   const results = await Promise.all(pCoordinates.map((c) => batchOneToManyCarRouting(c, pCoordinates, true)));
+    const results = await Promise.all(pCoordinates.map((c, idx) => batchOneToManyCarRouting(c, pCoordinates.filter((c2, idx2) => airDistanceCheck(pForward[0][idx]!,pBackward[0][idx2]!,leeways[0],c,c2)), true)));
    const r: Coordinates[][] = new Array<Coordinates[]>(pCoordinates.length);
+   for(let i=0;i!=r.length;++i) {
+        r[i] = new Array<Coordinates>();
+   }
    for(let i=0;i!=t.length-1;++i) {
-       for(let j=0;j!==pForward.length;++j) {
-        r[j] = new Array<Coordinates>();
-           const f = pForward[i][j];
-           for(let k=0;k!==pBackward.length;++k) {
-               const b = pBackward[i][k];
-               const pairDuration = results[j][k];
-               if(f === undefined || b === undefined || pairDuration === undefined || f + b + pairDuration > leeways[i]*1000) {
+       for(let j=0;j!==pForward[i].length;++j) {
+           const f = pForward[i][j]!;
+           let pos = -1;
+           for(let k=0;k!==pBackward[i].length;++k) {
+               if(airDistanceCheck(pForward[i][j]!,pBackward[i][k]!,leeways[0],pCoordinates[j],pCoordinates[k])){
+                   ++pos;
+               }else{
+                continue;
+               }
+               const b = pBackward[i][k]!;
+               const pairDuration = results[j][pos];
+               if(pairDuration === undefined || f + b + pairDuration > leeways[i]*1000) {
                    continue;
                }
                r[j].push(pCoordinates[k]);
@@ -112,7 +120,20 @@ async function readCoordinates(): Promise<Coordinates[]> {
 	return coordinates;
 }
 
-// Run the main function
+function airDistanceCheck(prevLeg: number, nextLeg: number, leeway: number, c: Coordinates, c2: Coordinates){
+        const dLat = c.lat - c2.lat;
+        const dLon = c.lng - c2.lng;
+        const metersPerDegLat = 111320;
+        const metersPerDegLon = 70000;
+        const distance = Math.sqrt(
+          (dLat * metersPerDegLat) ** 2 +
+          (dLon * metersPerDegLon) ** 2
+        );
+        const kmh = 80;
+        const travelTime = distance * HOUR/ 1000  / kmh;
+        return travelTime + prevLeg + nextLeg <= leeway * 1000
+}
+
 main().catch((error) => {
 	console.error('Error in main function:', error);
 });
